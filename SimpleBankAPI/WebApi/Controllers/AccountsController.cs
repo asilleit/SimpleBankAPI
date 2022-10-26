d8061a0e2291d538ce7e80163951039c8782d6eb
Adriano Leite <asilleite@emeal.nttdata.com>
2022-10-26 16:19:08 +0100

    Update Rotas
    Update Rotas

--- SimpleBankAPI/WebApi/Controllers/AccountsController.cs
+++ SimpleBankAPI/WebApi/Controllers/AccountsController.cs
@@ -54,7 +54,7 @@ namespace SimpleBankAPI.Controllers
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost(Name = "CreateAccount")]
         [Produces("application/json")]
-        [ProducesResponseType(typeof(AccountResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(AccountResponse), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
@@ -85,7 +85,7 @@ namespace SimpleBankAPI.Controllers
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public async Task<IActionResult> GetById(int id)
+        public async Task<IActionResult> GetById([FromRoute] int id)
         {
             try
             {
@@ -115,7 +115,116 @@ namespace SimpleBankAPI.Controllers
         {
             return (_context.Accounts?.Any(e => e.Id == id)).GetValueOrDefault();
         }
-    
 
+        // GET: api/Documents
+        [HttpGet("{id:int}/doc", Name = "ListDocuments")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult<IEnumerable<Document>>> GetDocumentsByAccount([FromRoute] int id)
+        {
+            try
+            {
+                int userId = int.Parse(_jwtAuth.GetClaim(Request.Headers.Authorization, "user"));
+               // int id = 23;  // 
+
+                var documento = await _documentsBusiness.GetById(id);
+                //Get Account ID
+                if (documento.AccountId != userId) return StatusCode(StatusCodes.Status401Unauthorized, "User don't have Owner from document");
+
+
+                var documents = await _documentsBusiness.GetDocumentsByAccount(userId);
+                var accountResponseList = DocumentResponse.FromListDocumentAccount(documents);
+                return StatusCode(StatusCodes.Status200OK, documents);
+            }
+            catch (Exception ex)
+            {
+                switch (ex)
+                {
+                    case AuthenticationException:
+                        return StatusCode(StatusCodes.Status401Unauthorized, ex.Message);
+                    case ArgumentException:
+                        return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+                    default:
+                        return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+                }
+            }
+        }
+
+        ////// GET: api/Documents/5
+        //[HttpGet("{id}")]
+        //[Produces("application/json")]
+        //[ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        //[ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        //[ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        ////[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        //public async Task<IActionResult> GetDocument(int id)
+        //{
+        //    try
+        //    {
+        //        int userId = int.Parse(_jwtAuth.GetClaim(Request.Headers.Authorization, "user"));
+        //        var documento = await _documentsBusiness.GetById(id);
+        //        var contaWithUser = await _accountsBusiness.GetById(documento.AccountId);
+        //        //Get Account ID
+        //        if (contaWithUser.UserId != userId) return StatusCode(StatusCodes.Status401Unauthorized, "User don't have Owner from document");
+
+        //        var document = await _context.Documents.FindAsync(id);
+
+        //        if (document == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        return StatusCode(StatusCodes.Status200OK, document);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        switch (ex)
+        //        {
+        //            case AuthenticationException:
+        //                return StatusCode(StatusCodes.Status401Unauthorized, ex.Message);
+        //            case ArgumentException:
+        //                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+        //            default:
+        //                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+        //        }
+        //    }
+        //}
+
+        // POST: api/Documents
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("{id:int}/doc", Name = "UploadDocument")]
+        [RequestSizeLimit(2 * 1024 * 1024)]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> PostDocument([FromRoute] int id)
+        {
+            try
+            {
+                var content = HttpContext.Request.Form.Files[0];
+                var account = await _accountsBusiness.GetById(id);
+                int userId = int.Parse(_jwtAuth.GetClaim(Request.Headers.Authorization, "user"));
+                var createdDocument = await _documentsBusiness.Create(content, account.Id, userId);
+                return StatusCode(StatusCodes.Status201Created, createdDocument);
+
+            }
+            catch (Exception ex)
+            {
+                switch (ex)
+                {
+                    case AuthenticationException:
+                        return StatusCode(StatusCodes.Status401Unauthorized, ex.Message);
+                    case ArgumentException:
+                        return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+                    default:
+                        return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+                }
+            }
+        }
     }
 }
