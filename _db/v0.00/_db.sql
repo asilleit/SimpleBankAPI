cb68071fee8b3414ad0ea1807fadc42d60c35477
Adriano Leite <asilleite@emeal.nttdata.com>
2023-01-06 13:21:07 +0000

    DatabaseV2
    DatabaseV2

--- _db/v0.00/_db.sql
+++ _db/v0.00/_db.sql
@@ -1,53 +1,13 @@
-
---DROP TABLE IF EXISTS public."Transfer";
---DROP TABLE IF EXISTS public."Movements";
---DROP TABLE IF EXISTS public."Accounts";
---DROP TABLE IF EXISTS public."Tokens";
---DROP TABLE IF EXISTS public."Users";
---DROP TABLE IF EXISTS public."Documents";
+DROP TABLE IF EXISTS public."Transfer";
+DROP TABLE IF EXISTS public."Accounts";
+DROP TABLE IF EXISTS public."Tokens";
+DROP TABLE IF EXISTS public."Users";
+DROP TABLE IF EXISTS public."Documents";
 
 
--- public."Documents" definition
 
--- Drop table
-
--- DROP TABLE public."Documents";
-
-CREATE TABLE public."Documents" (
-	id serial4 NOT NULL,
-	file_name varchar NOT NULL,
-	file_type varchar NOT NULL,
-	file bytea NOT NULL,
-	account_id int4 NOT NULL,
-	created_at timestamptz NOT NULL DEFAULT now(),
-	CONSTRAINT documents_pkey PRIMARY KEY (id)
-);
-
-
--- public."Transfer" definition
-
--- Drop table
-
--- DROP TABLE public."Transfer";
-
-CREATE TABLE public."Transfer" (
-	id serial4 NOT NULL,
-	amount numeric NOT NULL,
-	fromaccountid int4 NOT NULL,
-	toaccountid int4 NOT NULL,
-	created_at timestamptz NOT NULL DEFAULT now(),
-	CONSTRAINT transfers_pkey PRIMARY KEY (id)
-);
-
-
--- public."Users" definition
-
--- Drop table
-
--- DROP TABLE public."Users";
-
-CREATE TABLE public."Users" (
-	id serial4 NOT NULL,
+CREATE TABLE "Users" (
+	id serial NOT NULL,
 	username varchar NOT NULL,
 	"password" varchar NOT NULL,
 	full_name varchar NOT NULL,
@@ -55,56 +15,47 @@ CREATE TABLE public."Users" (
 	password_changed_at timestamptz NOT NULL DEFAULT now(),
 	created_at timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT users_email_key UNIQUE (email),
-	CONSTRAINT users_pkey PRIMARY KEY (id),
+		CONSTRAINT users_pkey PRIMARY KEY (id),
 	CONSTRAINT users_username UNIQUE (username)
 );
 
-
--- public."Accounts" definition
-
--- Drop table
-
--- DROP TABLE public."Accounts";
-
-CREATE TABLE public."Accounts" (
-	id serial4 NOT NULL,
-	user_id int4 NOT NULL,
-	balance numeric NOT NULL,
+CREATE TABLE "Accounts" (
+	id serial NOT NULL,
+	user_id int NOT NULL,
+	balance decimal NOT NULL,
 	currency varchar NOT NULL,
 	created_at timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT accounts_pkey PRIMARY KEY (id),
-	CONSTRAINT accounts_fkey FOREIGN KEY (user_id) REFERENCES public."Users"(id)
+	CONSTRAINT accounts_fkey FOREIGN KEY(user_id) REFERENCES "Users"(id)
 );
 
 
--- public."Movements" definition
-
--- Drop table
-
--- DROP TABLE public."Movements";
-
-CREATE TABLE public."Movements" (
-	id serial4 NOT NULL,
-	accountid int4 NOT NULL,
-	amount numeric NOT NULL,
+create table "Transfer" (
+	id serial NOT NULL,
+	amount decimal NOT NULL,
+	fromAccountId int NOT NULL,
+	toAccountId int NOT NULL,
 	created_at timestamptz NOT NULL DEFAULT now(),
-	CONSTRAINT movements_pkey PRIMARY KEY (id),
-	CONSTRAINT movements_fkey FOREIGN KEY (accountid) REFERENCES public."Accounts"(id)
+	CONSTRAINT transfers_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE "Tokens" (
+   	id serial NOT NULL,
+    user_id int NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    refresh_token varchar,
+    refresh_token_expire_at timestamptz,
+    CONSTRAINT tokens_pkey PRIMARY KEY (id),
+    CONSTRAINT tokens_fkey FOREIGN KEY(user_id) REFERENCES "Users"(id) 
 );
 
 
--- public."Tokens" definition
-
--- Drop table
-
--- DROP TABLE public."Tokens";
-
-CREATE TABLE public."Tokens" (
-	id serial4 NOT NULL,
-	user_id int4 NOT NULL,
+CREATE TABLE "Documents" (
+   	id serial NOT NULL,
+	file_name varchar NOT NULL,
+	file_type varchar NOT NULL,
+	file bytea NOT NULL,
+	account_id int NOT NULL,
 	created_at timestamptz NOT NULL DEFAULT now(),
-	refresh_token varchar NULL,
-	refresh_token_expire_at timestamptz NULL,
-	CONSTRAINT tokens_pkey PRIMARY KEY (id),
-	CONSTRAINT tokens_fkey FOREIGN KEY (user_id) REFERENCES public."Users"(id)
+	CONSTRAINT documents_pkey PRIMARY KEY (id)
 );
